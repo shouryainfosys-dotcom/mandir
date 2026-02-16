6364f917cc6fe1c4f04c752af431678e55bbe0b3
shouryainfosys-dotcom <shouryainfosys@gmail.com>
2026-02-16 16:35:01 +0530

    Delete app.tsx

--- app.tsx
+++
@@ -1,4 +0,0 @@
-
-// This file is cleared to resolve casing conflicts between 'App.tsx' and 'app.tsx'.
-// Standard React PascalCase 'App.tsx' is now used for the main application logic.
-export {};
